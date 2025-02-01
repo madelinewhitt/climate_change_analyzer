97830cf470abd1afd34587e4f331334bd50f398c
madelinewhitt <madelinewhitton@MacBook-Air-10.local>
2025-02-01 12:22:58 -0500

    Created a function that interacts with the api properly

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
 import requests 
 import json
 import os
-
+import io
 from dotenv import load_dotenv, dotenv_values
+import pandas as pd
 
 load_dotenv()
 
-print(os.getenv("api_key"))
+def interact_api():
+    # URL of the IBTrACS dataset (Global CSV)
+    url = "https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/csv/ibtracs.ALL.list.v04r00.csv"
 
+    # Download file
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Response status code:", response.status_code)
+        # Load dataset into Pandas using StringIO
+        df = pd.read_csv(io.StringIO(response.text), low_memory=False)
+
+        # Display first 5 rows
+        print(df.head())
+    else:
+        print(f"Error {response.status_code}: Unable to fetch data.")
+
+# Call the function
+interact_api()
