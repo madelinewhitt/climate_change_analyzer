f4a3d26944929b4afb0cd4c68d56225efeb1b356
bobromero <rromero123102@gmail.com>
2025-02-22 16:07:59 -0500

    flub

--- src/data_processor.py
+++ src/data_processor.py
@@ -4,5 +4,8 @@ file_name = "../data/naturalDisaster2000-2024(EM-DAT Data).csv"
 
 df = pd.read_csv(file_name, encoding='ISO-8859-1')
 # print(df.info())
+count = 0
 for d in df['Disaster Type']:
-    print(d == 'Flood')
+    if d=='Flood':
+        count += 1
+print(count)
