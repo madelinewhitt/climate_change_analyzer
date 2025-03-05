020fca1b6540ea8d817e0fe8cbc59199d824972a
bobromero <rromero123102@gmail.com>
2025-03-05 17:19:48 -0500

    deal with new data

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-file_name = "../data/naturalDisaster2000-2024(EM-DAT Data).csv"
+file_name = "../data/NaturalDisasters1900-2025.csv"
 
-df = pd.read_csv(file_name, encoding='ISO-8859-1')
-# print(df.info())
-count = 0
-for d in df['Disaster Type']:
-    if d=='Flood':
-        count += 1
-print(count)
+def getData():
+    df = pd.read_csv(file_name, encoding='ISO-8859-1')
+    print(df)
+    for d in df['Disaster Type']:
+        pass
+
+if __name__ == "__main__":
+    #anything that is not a function will be here
+    getData()
