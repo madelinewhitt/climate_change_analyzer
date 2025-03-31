f2e923728bd74e43394d7de0aa0806f0092cbd02
bobromero <rromero123102@gmail.com>
2025-03-31 19:14:13 -0400

    bruh

--- src/pred.py
+++ src/pred.py
@@ -10,19 +10,24 @@ future_years = np.arange(2025, 2035)
 future_months = np.tile(np.arange(1, 13), len(future_years))
 
 
-future_latitudes = np.random.uniform(df['Latitude'].min(), df['Latitude'].max(), len(future_years) * 12)
-future_longitudes = np.random.uniform(df['Longitude'].min(), df['Longitude'].max(), len(future_years) * 12)
+future_latitudes = np.random.uniform(
+    df["Latitude"].min(), df["Latitude"].max(), len(future_years) * 12
+)
+future_longitudes = np.random.uniform(
+    df["Longitude"].min(), df["Longitude"].max(), len(future_years) * 12
+)
 
 
-future_df = pd.DataFrame({
-    'Start Year': np.repeat(future_years, 12),
-    'Start Month': future_months,
-    'Latitude': future_latitudes,
-    'Longitude': future_longitudes
-})
+future_df = pd.DataFrame(
+    {
+        "Start Year": np.repeat(future_years, 12),
+        "Start Month": future_months,
+        "Latitude": future_latitudes,
+        "Longitude": future_longitudes,
+    }
+)
 
 
+future_df["Predicted Magnitude"] = model.predict(future_df)
 
-future_df['Predicted Magnitude'] = model.predict(future_df)
-
-print(future_df.head())
+print(future_df.to_string())
