162d58ac9788841ece1b4a46fde78153cacac859
bobromero <rromero123102@gmail.com>
2025-04-10 12:05:40 -0400

    changed import to work with tests

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,4 +1,4 @@
-from data_processor import load_disaster
+from src.data_processor import load_disaster
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
