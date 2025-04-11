afe180ed62dd9c3307222e5301df208ad3ff99ff
bobromero <rromero123102@gmail.com>
2025-04-11 12:45:06 -0400

    removed comment

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ mkdir data/generated_data
 cd src
 python3 data_processor.py
 python3 algorithms.py 
-python3 multi_algorithms.py #Needs fixing
+python3 multi_algorithms.py
 python3 time_series.py 
 python3 clust.py
 #Madeline python3 src/visualizer.py
